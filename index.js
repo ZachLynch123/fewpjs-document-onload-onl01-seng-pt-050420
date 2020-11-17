81d24ed1a1fea5eaf5a9be2418c211a2dc5a3769
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-11-17 18:05:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 
 document.addEventListener("DOMContentLoaded", () => {
-  let text = document.getElementById("")
+  let text = document.getElementById("text")
 });
 
 console.log(
